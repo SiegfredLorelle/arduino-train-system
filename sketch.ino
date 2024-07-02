2dfb2e27c992265da59550df659b4e6c712cf7e5
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-07-02 22:25:49 +0800

    Read IR signals

--- sketch.ino
+++ sketch.ino
@@ -1,14 +1,14 @@
-#include <LCD_I2C.h> 
+#include <LiquidCrystal_I2C.h> 
 
-LCD_I2C lcdStation1(0x27, 16, 2);
-LCD_I2C lcdStation2(0x26, 16, 2);
-LCD_I2C lcdStation3(0x25, 16, 2);
-LCD_I2C lcdStation4(0x24, 16, 2);
-LCD_I2C lcdStation5(0x23, 16, 2);
-LCD_I2C lcdStation6(0x22, 16, 2);
+LiquidCrystal_I2C lcdStation1(0x27, 16, 2);
+LiquidCrystal_I2C lcdStation2(0x26, 16, 2);
+LiquidCrystal_I2C lcdStation3(0x25, 16, 2);
+LiquidCrystal_I2C lcdStation4(0x24, 16, 2);
+LiquidCrystal_I2C lcdStation5(0x23, 16, 2);
+LiquidCrystal_I2C lcdStation6(0x22, 16, 2);
 
 const int LCDS_SIZE = 6;
-const LCD_I2C LCDS[LCDS_SIZE] = {
+LiquidCrystal_I2C LCDS[LCDS_SIZE] = {
   lcdStation1,
   lcdStation2,
   lcdStation3,
@@ -17,25 +17,58 @@ const LCD_I2C LCDS[LCDS_SIZE] = {
   lcdStation6,
 };
 
+int IR1 = 8; 
+int IR2 = 9; 
+int IR3 = 10; 
+int IR4 = 11; 
+int IR5 = 12; 
+int IR6 = 13; 
+
+const int IRS_SIZE = 6;
+const int IRS[IRS_SIZE] = {
+  IR1,
+  IR2,
+  IR3,
+  IR4,
+  IR5,
+  IR6,
+};
 
 
 void setup()
 {
   Serial.begin(9600);
   initLcds();
+  initIrs();
 }
- 
- 
+
+
 void loop()
 {
-
+  readIrSignals();
+  delay(10);
 }
 
 void initLcds() {
   for (int i = 0; i < LCDS_SIZE; i++) {
-    LCDS[i].begin();
+    LCDS[i].init();
     LCDS[i].backlight();
     LCDS[i].print("Station ");
     LCDS[i].print(i + 1);
   }
+}
+
+void initIrs() {
+  for (int i = 0; i < IRS_SIZE; i++) {
+    pinMode(IRS[i], INPUT);
+  }
+}
+
+void readIrSignals() {
+  Serial.print("IR Signals: ");
+    for (int i = 0; i < IRS_SIZE; i++) {
+      int currSignal = digitalRead(IRS[i]);
+      Serial.print(currSignal);
+    }
+  Serial.println();
 }
