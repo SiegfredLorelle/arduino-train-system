c54f249deb318f86bff5846e352351749effd6e1
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-07-04 10:35:19 +0800

    Find occupied stations at the start

--- sketch.ino
+++ sketch.ino
@@ -1,14 +1,15 @@
 #include <LiquidCrystal_I2C.h> 
 
+const int STATIONS_SIZE = 6;
+const int TRAINS_SIZE = 3;
+
 LiquidCrystal_I2C lcdStation1(0x27, 16, 2);
 LiquidCrystal_I2C lcdStation2(0x26, 16, 2);
 LiquidCrystal_I2C lcdStation3(0x25, 16, 2);
 LiquidCrystal_I2C lcdStation4(0x24, 16, 2);
 LiquidCrystal_I2C lcdStation5(0x23, 16, 2);
 LiquidCrystal_I2C lcdStation6(0x22, 16, 2);
-
-const int LCDS_SIZE = 6;
-LiquidCrystal_I2C LCDS[LCDS_SIZE] = {
+LiquidCrystal_I2C LCDS[STATIONS_SIZE] = {
   lcdStation1,
   lcdStation2,
   lcdStation3,
@@ -23,8 +24,7 @@ int IR3 = 10;
 int IR4 = 11; 
 int IR5 = 12; 
 int IR6 = 13; 
-const int IRS_SIZE = 6;
-const int IRS[IRS_SIZE] = {
+const int IRS[STATIONS_SIZE] = {
   IR1,
   IR2,
   IR3,
@@ -36,11 +36,20 @@ const int IRS[IRS_SIZE] = {
 int motor1 = 5;
 int motor2 = 6;
 int motor3 = 7;
-const int MOTORS_SIZE = 3;
-const int MOTORS[MOTORS_SIZE] = {
+const int MOTORS[TRAINS_SIZE] = {
   motor1,
   motor2,
   motor3,
+};
+
+
+int areStationsOccupied[STATIONS_SIZE] = {
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
 };
 
 
@@ -49,19 +58,22 @@ void setup()
   Serial.begin(9600);
   initLcds();
   initIrs();
+  initMotors();
+
+  findOccupiedStations();
 }
 
 
 void loop()
 {
   // readIrSignals();
-  testMotors();
-
+  // testMotors();
+  findOccupiedStations(); // Prolly sa setup dapat to
   delay(10);
 }
 
 void initLcds() {
-  for (int i = 0; i < LCDS_SIZE; i++) {
+  for (int i = 0; i < STATIONS_SIZE; i++) {
     LCDS[i].init();
     LCDS[i].backlight();
     LCDS[i].print("Station ");
@@ -70,20 +82,20 @@ void initLcds() {
 }
 
 void initIrs() {
-  for (int i = 0; i < IRS_SIZE; i++) {
+  for (int i = 0; i < STATIONS_SIZE; i++) {
     pinMode(IRS[i], INPUT);
   }
 }
 
 void initMotors() {
-  for (int i = 0; i < MOTORS_SIZE; i++) {
+  for (int i = 0; i < TRAINS_SIZE; i++) {
     pinMode(MOTORS[i], OUTPUT);
   }
 }
 
 void readIrSignals() {
   Serial.print("IR Signals: ");
-    for (int i = 0; i < IRS_SIZE; i++) {
+    for (int i = 0; i < STATIONS_SIZE; i++) {
       int currSignal = digitalRead(IRS[i]);
       Serial.print(currSignal);
     }
@@ -91,10 +103,28 @@ void readIrSignals() {
 }
 
 void testMotors() {
-  for (int i = 0; i < MOTORS_SIZE; i++) {
+  for (int i = 0; i < TRAINS_SIZE; i++) {
     digitalWrite(MOTORS[i], HIGH);
     delay(5000);
     digitalWrite(MOTORS[i], LOW);
     delay(1000);
   }
+}
+
+void findOccupiedStations() {
+  for (int i = 0; i < STATIONS_SIZE; i++) {
+    int currSignal = digitalRead(IRS[i]);
+    if (currSignal == HIGH) {
+      areStationsOccupied[i] = true;
+    }
+  }
+  printArray("STATIONS: ", areStationsOccupied, STATIONS_SIZE);
+}
+
+void printArray(char* message, int arr[], int size) {
+  Serial.print(message);
+  for (int i = 0; i < size; i++) {
+    Serial.print(arr[i]);
+  }
+  Serial.println();
 }
